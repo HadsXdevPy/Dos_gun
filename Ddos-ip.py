e78e03bef47d03e81dad9d09a468d4cbb73469a0
SubangProgramer <71500254+SubangProgramer@users.noreply.github.com>
2020-11-02 18:24:56 +0700

    Add files via upload

---
+++ Ddos-ip.py
@@ -0,0 +1,21 @@
+#!/user/bin/python
+import time
+import os
+import sys
+
+os.system('clear')
+os.system('figlet Ddos ip')
+print ""
+print "Recode lasung gua dos :v"
+print "biodata author:"
+print  ""
+print "NAMA:HADI"
+print "ASAL:SUBANG"
+print "GURU:Ricko En"
+print "TERMINAL:Teminal Linux"
+print "SEKOLAH:Nurul huda"
+print ""
+a = raw_input('Target IP :')
+for Dos in range(1001):
+ time.sleep(0.1)
+ print Dos,"attacking to",a
